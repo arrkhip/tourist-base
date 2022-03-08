b7878b90ad544e733948c7b49c9621b3e63d0cc8
Vladislav <arkhipovvladislav@mai.ru>
2022-03-08 20:16:22 +0300

    дописал функционал шапки

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,7 +1,18 @@
-const buttonMenu = document.querySelector('.j_header-toggle');
+const buttonOpenMenu = document.querySelector('.j_header-toggle');
 const menu = document.querySelector('.j_menu');
 
-buttonMenu.addEventListener('click', function() {
-  this.classList.toggle('active');
+const toggleMobileMenu = () => {
+  buttonOpenMenu.classList.toggle('active');
   menu.classList.toggle('active');
+}
+
+buttonOpenMenu.addEventListener('click', toggleMobileMenu); 
+
+
+// Закрытие меню при клике вне меню
+window.addEventListener('click', (e) => { 
+  const target = e.target;
+  if (!target.closest('.header__inner') && !target.closest('.j_header-toggle')) { 
+    menu.classList.remove('active');
+  }
 });
